9079d0c7e59404b5e64d51440660989be6b8aa9e
Keegan Brunmeier <keeganbrunmeier@gmail.com>
2022-05-04 12:45:41 -0400

    Added comments to html image tag generator

--- downloads/html-image-tag-generator.py
+++ downloads/html-image-tag-generator.py
@@ -1,15 +1,18 @@
 from PIL import Image
-from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import pyperclip
 
-Tk().withdraw()
+#File user prompt
 filename = askopenfilename()
 
+#Open file
 im = Image.open(filename)
 
+#Assess width and height
 w, h = im.size
 
+#Generate string
 imageTag = f"<img src=\"{filename}\", width=\"{w}\", height=\"{h}\", alt=\"\">"
 
+#Copy to clipboard
 pyperclip.copy(imageTag)
